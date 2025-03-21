dcec1a7dd379d8fb121f3a52b59647b31dfa16a8
Tronken <140592825+Tronken@users.noreply.github.com>
2025-03-21 21:09:45 +0100

    inventory

---
+++ Assets/scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject InventoryMenu;
+    private bool menuActivated;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Inventory") && menuActivated)
+        {
+            Time.timeScale = 1;
+            InventoryMenu.SetActive(false);
+            menuActivated = false;
+        }
+        
+        else if (Input.GetButtonDown("Inventory") && !menuActivated)
+        {
+            Time.timeScale = 0;
+            InventoryMenu.SetActive(true);
+            menuActivated = true;
+        }
+    }
+}
