1a07eb42e8421c102071f9bcf24a48edc47c4594
Tronken <140592825+Tronken@users.noreply.github.com>
2025-03-27 18:59:18 +0100

    Item.Script.innan.Chatgpt

---
+++ Assets/scripts/Inventory/Item.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] private string itemName;
+
+    [SerializeField] private int quantity;
+
+    [SerializeField] private Sprite sprite;
+
+    private InventoryManager inventoryManager;
+    void Start()
+    {
+        inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            inventoryManager.AddItem(itemName, quantity, sprite);
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddItem(string itemName, int quantity, Sprite itemSprite)
+    {
+        Debug.Log("itemName = " + itemName + "quantity = " + quantity + "itemSprite = " + itemSprite);
+    }
+}
